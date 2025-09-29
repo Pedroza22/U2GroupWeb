128dae270345e3428d505e8f4c2e0abab07e7638
pedroza22 <julianpedrozaospina@gmail.com>
2025-09-29 15:31:46 -0500

    Fix routes-manifest.json error: update vercel.json to use npm and remove outputFileTracingRoot from next.config.mjs

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -7,7 +7,6 @@ const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingRoot: path.join(__dirname, '../'),
   images: {
     unoptimized: true,
     domains: [
